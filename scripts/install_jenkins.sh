d01463f27fc6cdf75765a7a16c579c3710057633
roiderra <46687205+roiderra@users.noreply.github.com>
2021-04-07 22:10:43 +0300

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -22,6 +22,4 @@ docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
-echo 'Administrator password is'
-sudo cat /var/jenkins_home/secrets/initialAdminPassword
 echo 'You need to reboot system to use docker commands without sudo'
